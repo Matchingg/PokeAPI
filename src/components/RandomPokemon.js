31e96ae90dcf2f72f9f11d2f702cdb13987be7a3
Matthew Ch'ng <mattchng03@gmail.com>
2022-06-10 14:16:43 +1000

    generates random pokemon with name and sprite

--- src/components/RandomPokemon.js
+++ src/components/RandomPokemon.js
@@ -5,6 +5,8 @@ export default function RandomPokemon() {
   const [loading, setLoading] = useState(true);
   const [maxId, setMaxId] = useState(0);
   const [randomId, setRandomId] = useState("");
+  const [pokemonSprite, setPokemonSprite] = useState("");
+  const [pokemonName, setPokemonName] = useState("");
 
   useEffect(() => {
     setLoading(true);
@@ -26,15 +28,34 @@ export default function RandomPokemon() {
     setRandomId(Math.floor(Math.random() * maxId) + 1);
   }
 
+  useEffect(() => {
+    setLoading(true);
+    const PokemonIdCall = `https://pokeapi.co/api/v2/pokemon/${randomId}`;
+    let cancel;
+    axios
+      .get(PokemonIdCall, {
+        cancelToken: new axios.CancelToken((c) => (cancel = c)),
+      })
+      .then((res) => {
+        setLoading(false);
+        setPokemonName(res.data.name);
+        setPokemonSprite(res.data.sprites.front_default);
+      });
+    return () => cancel();
+  }, [randomId]);
+
   if (loading) return "Loading...";
 
   return (
     <>
       <div>
-        <h1>Hello world</h1>
+        <h1>Pokemon Generator</h1>
       </div>
-      <div>{randomId}</div>
-      <button onClick={generateRandom}>Generate New Pokemon ID</button>
+      <div>{pokemonName.toUpperCase()}</div>
+      <div>
+        <img src={pokemonSprite} alt={pokemonName} height="200px" />
+      </div>
+      <button onClick={generateRandom}>Generate New Pokemon</button>
     </>
   );
 }
